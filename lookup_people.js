2c5ce091e2d57c21fa51f9e404c6bb445da53330
Adam <adamqkc@gmail.com>
2018-08-30 05:23:26 -0700

    implement getPeopleByFirstName function

--- lookup_people.js
+++ lookup_people.js
@@ -11,22 +11,30 @@ const client = new pg.Client({
 });
 
 client.connect((err) => {
+  const first_name = process.argv[2];
+  
   if (err) {
     return console.error("Connection Error", err);
   }
-  
-  const first_name = process.argv[2];
 
-  client.query(`
+  function getPeopleByFirstName(first_name) {
+    console.log('Searching...');
+  
+    client.query(`
     select * 
     from famous_people as people 
     where people.first_name = $1::text`, [first_name], (err, result) => {
-    
       if (err) {
-      return console.error("error running query", err);
+        console.log('Error: ', err);
       }
-    
-      console.log(result.rows);
+  
+      console.log(`Found ${result.rows.length} person(s) by the name '${first_name}':`)
+      
+      result.rows.forEach(row => {
+        console.log(`- ${row.id}: ${row.first_name} ${row.last_name}, born ${row.birthdate.toLocaleDateString()}`);
+      });
       client.end();
     });
+  }
+  getPeopleByFirstName(first_name);
 }); 
