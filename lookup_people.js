8242ea908712beba7235d218f854b8fac5bd40fe
Adam <adamqkc@gmail.com>
2018-08-31 03:36:19 -0700

    make migrations file that adds a foreign key to milestones

--- lookup_people.js
+++ lookup_people.js
@@ -3,7 +3,6 @@ const settings = require("./settings"); // settings.json
 
 const client = new pg.Client({
   user: settings.user,
-  password: settings.password,
   database: settings.database,
   host: settings.hostname,
   port: settings.port,
