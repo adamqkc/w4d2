d1c1feefbc71182486c71430e839e615bd6f0afe
Adam <adamqkc@gmail.com>
2018-08-30 22:43:57 -0700

    update dependencies and finish knex function

---
+++ add_person.js
@@ -0,0 +1,25 @@
+const settings = require('./settings.json')
+
+const first_name = process.argv[2];
+const last_name = process.argv[3];
+const birthdate = process.argv[4];
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user: settings.user,
+    database: settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
+  }
+});
+
+knex('famous_people').insert({ 
+  first_name: `${first_name}`, 
+  last_name:  `${last_name}`, 
+  birthdate:  `${birthdate}` 
+  })
+  .then(() => {
+    console.log(`${first_name} ${last_name} successfully added!`);
+  })
